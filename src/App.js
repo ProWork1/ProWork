0e59fc611ba427e64b6435413be979c86ed1ffc8
ElyorU <eldiyorxonxojaumarov021@gmail.com>
2021-07-14 17:20:40 +0500

    contact

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import './App.css'
 import Navbar from './companent/Navbar'
 import Footer from './companient/Footer'
-<<<<<<< HEAD
 import SignIn from './companient2/SignIn'
+import Contact from './companent/Contact'
 
 function App() {
     return <div className='App'>
-        <SignIn />
-        <Footer />
+        {/* <SignIn /> */}
+        {/* <Footer /> */}
+        <Contact />
     </div>
 }
-=======
-
-function App() {
-    return <div className='App'>
-        <Navbar />
-</div>}
-
->>>>>>> 00fd36919621962e3c8c24ccd9c961bf193d7e2b
-
 export default App
