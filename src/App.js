17d5e7eb49a9fb5e690f04dcfd4d7bc9b55e52ed
MurodkhonJalolov <murodkhanjalolov2004@gmail.com>
2021-07-14 17:01:11 +0500

    porfolio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css'
+import Portfolio from './component/Portfolio'
 
 function App() {
-    return <div className='App'></div>
+    return <div className='App'>
+        <Portfolio />
+    </div>
 }
 
 export default App
