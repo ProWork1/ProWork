00fd36919621962e3c8c24ccd9c961bf193d7e2b
lazizburonov <lazizburonov900800858@gmail.com>
2021-07-14 14:54:51 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import './App.css'
-<<<<<<< HEAD
 import Navbar from './companent/Navbar'
+import Footer from './companient/Footer'
 
 function App() {
     return <div className='App'>
         <Navbar />
-=======
-import Footer from './companient/Footer'
+</div>}
 
-
-function App() {
-    return <div className='App'>
-        <Footer />
->>>>>>> b425b7be1292a5899968dc07453bf57b364d4ddb
-    </div>
-}
 
 export default App
